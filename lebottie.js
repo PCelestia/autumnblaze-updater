13c161e12f3cf4a5014e02b7cb809a5a31172b9f
Alexander Liu <liujalexander84@gmail.com>
2020-07-10 13:39:29 -0700

    make messages better, ignore thebot.js (test)

--- lebottie.js
+++ lebottie.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const { exec } = require("child_process");
 
-// or update
+// first time
 const install = () => {
    console.log("==================== GIT CLONEING ====================");
    exec("git clone https://github.com/PCelestia/autumnblaze.git", (err, stdo, stde) => {
@@ -15,29 +15,33 @@ const install = () => {
       }
       console.log("==================== GIT CLONE OUTPUT ====================");
       console.log(stdo);
-      update();
+      update(true);
    });
 };
-const update = () => {
+
+// move to autumnblaze if arent there already
+const movedirs = () => {
    if (!process.cwd().endsWith("autumnblaze")) try {
       process.chdir("./autumnblaze");
    } catch (err) {
-      console.warn("failed to update: could not switch working directory");
+      return false;
+   }
+   return true;
+};
+
+// update the repo and npm install
+const update = (e = false) => {
+   // if e is true, its called from install lol
+   // if its false, its an update
+   // quite strange ik
+   if (!movedirs()) {
+      if (e) console.warn("failed to install: could not switch working directory");
+      else console.warn("failed to update: could not switch working directory");
       return;
    }
-   console.log("==================== GIT PULLING ====================");
-   exec("git pull", (err, stdo, stde) => {
-      if (err) {
-         console.warn("==================== ERROR ====================\n" + err);
-         return;
-      }
-      if (stde) {
-         console.warn("==================== STDERROR ====================\n" + stde);
-      }
-      console.log("==================== GIT PULL OUTPUT ====================");
-      console.log(stdo)
-      console.log("==================== NPM INSTALLING ====================");
-      exec("npm install", (err, stdo, stde) => {
+   if (!e) {
+      console.log("==================== GIT PULLING ====================");
+      exec("git pull", (err, stdo, stde) => {
          if (err) {
             console.warn("==================== ERROR ====================\n" + err);
             return;
@@ -45,16 +49,35 @@ const update = () => {
          if (stde) {
             console.warn("==================== STDERROR ====================\n" + stde);
          }
-         console.log("==================== NPM INSTALL OUTPUT ====================");
+         console.log("==================== GIT PULL OUTPUT ====================");
          console.log(stdo);
+         npminstall(e);
       });
-   });
+   } else npminstall(e);
 };
 
-try {
-   fs.access("./autumnblaze");
-} catch (err) {
-   // likely cause not installed yet
-   console.log("==================== FIRST TIME INSTALLING ====================");
-   install();
-}
+// install npm dependencies
+const npminstall = (e) => {
+   console.log("==================== NPM INSTALLING ====================");
+   exec("npm install", (err, stdo, stde) => {
+      if (err) {
+         console.warn("==================== ERROR ====================\n" + err);
+         return;
+      }
+      if (stde) {
+         console.warn("==================== STDERROR ====================\n" + stde);
+      }
+      console.log("==================== NPM INSTALL OUTPUT ====================");
+      console.log(stdo);
+      if (!e) console.log("update success");
+   });
+};
+// see if the directory already exists
+// if it does, assuming already cloned and npm installed
+module.exports = () => {
+   if (!fs.existsSync("./autumnblaze")) {
+      console.log("==================== FIRST TIME INSTALLING ====================");
+      console.log("\n=============== HAIIIIII WELCOME TO MY THING LOL ==============");
+      install();
+   } else update();
+};
